4b00087d03edc70b809819f8f231424a073511ec
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2022-12-08 09:15:53 -0500

    init(): create-stencil v3.1.1 output

---
+++ stencil.config.ts
@@ -0,0 +1,21 @@
+import { Config } from '@stencil/core';
+
+export const config: Config = {
+  namespace: 'run-on-ubuntu-twenty-two',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+  ],
+};
